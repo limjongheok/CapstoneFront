aa2ef77a7ee8b211b821a5903e0d41f5a07d374a
sinbak <sinbato03@daum.net>
2023-05-26 01:43:01 +0900

    feature : Implement EmailAuthrequ.js

---
+++ src/pages/EmailAuthrequ.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const Emailauthrequ=()=>{
+	return(
+		<Container component="main" maxWidth="sm">
+      <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+      >
+        <Typography component="h1" variant="h6" marginBottom="96px" fontSize="24px">
+            한남대학교 학교 이메일 인증을 진행합니다.
+        </Typography>
+        </Box>
+    <Box sx={{
+              maxWidth:"385px",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              margin: "0 auto"
+            }}>
+		<TextField 
+        id="standard-email-input"
+        sx={{mb:"24px"}}
+        label="학교 이메일" 
+        required 
+        fullWidth
+        name="email"
+        autoComplete="email"
+        variant="standard"
+        autoFocus
+        />
+      
+      <Button type="submit" fullWidth variant="contained"
+      sx={{mt:3, mb:2, backgroundColor:"black", height:"48px"}}>
+        인증요청 
+      </Button>
+      </Box>
+		</Container>
+	)
+}
+
+export default Emailauthrequ
